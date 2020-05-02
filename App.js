61d2b75900c23cc4460e0682ca431604c2e8138c
kavindasilva <kss.kavinda@gmail.com>
2020-05-02 16:06:14 +0530

    WIP login ui started

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
+import Login from "./src/login/login"
+
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
-  );
+export default function App(){
+	return <Login />
 }
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.welcome}>Welcome to React Native!</Text>
+//       <Text style={styles.instructions}>To get started, edit App.js</Text>
+//       <Text style={styles.instructions}>{instructions}</Text>
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
