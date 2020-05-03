3ed02792846ef65edf0dda687041576aac4b325b
kavindasilva <kss.kavinda@gmail.com>
2020-05-03 19:24:49 +0530

    code clean

--- App.js
+++ App.js
@@ -1,56 +1,23 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 
-import Login from "./src/login/login"
-import Routes from "./src/router";
+// import Login from "./src/login/login"
+// import Routes from "./src/router";
 
-import SideMenu from "react-native-side-menu";
-// import SideMecnu from "@rre";
-// import SideMenuContent from "./src/sideBar/sideMenu"
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+
+import SideMenuContent from "./src/sideBar/reactNavSideMenu"
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
-
-const Drawer = createDrawerNavigator();
-
-function Application() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
-}
 
 export default function App(){
   // return <Login />
   // return <Routes />
-  return <Application />
+  // return <Application />
+  return <SideMenuContent />
 }
 
 const styles = StyleSheet.create({
