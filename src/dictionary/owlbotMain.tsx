1d4a00b69c6e7db5c44fc39e1f9b7a33e98e6a2d
kavindasilva <kss.kavinda@gmail.com>
2020-05-08 10:35:16 +0530

    WIP scrolling

--- src/dictionary/owlbotMain.tsx
+++ src/dictionary/owlbotMain.tsx
@@ -19,58 +19,58 @@ export default function OwlBotDictionary({ navigation }) {
     const [ isLoading, setIsLoading ] = useState(false);
 
     return (
-            <ScrollView>
+        <ScrollView style={{flex:1}} >
             <View style={styles.container}>
-            <Image style={{height:30, width:30}} source={{uri: 'http://africhthy.org/sites/africhthy.org/files/styles/thumbnail/public/garland_logo.png'}} />
-            {/* <Image style={{height:30, width:30}} source={{uri: 'https://media.owlbot.info/dictionary/images/owl.jpg.400x400_q85_box-403,83,960,640_crop_detail.jpg'}} /> */}
+                <Image style={{height:30, width:30}} source={{uri: 'http://africhthy.org/sites/africhthy.org/files/styles/thumbnail/public/garland_logo.png'}} />
+                {/* <Image style={{height:30, width:30}} source={{uri: 'https://media.owlbot.info/dictionary/images/owl.jpg.400x400_q85_box-403,83,960,640_crop_detail.jpg'}} /> */}
 
-            <Text style={styles.welcome}>Welcome to OwlBot API consumer</Text>
+                <Text style={styles.welcome}>Welcome to OwlBot API consumer</Text>
 
-            <Text>Add search Item</Text>
-            <TextInput
-                placeholder="enter search word"
-                value={ searchQuery }
-                onChangeText={ (txt)=>setSearchQuery(txt) }
-            />
-            <Button
-                onPress={ async () => {
-                    setIsLoading(true);
-                    let xx = await searchApi(searchQuery);
-                    console.debug("xx:",xx);
-                    setWordDefinition(xx.definitions)
-                    setIsLoading(false);
-                }}
-                title={"Search"}
-            />
+                <Text>Add search Item</Text>
+                <TextInput
+                    placeholder="enter search word"
+                    value={ searchQuery }
+                    onChangeText={ (txt)=>setSearchQuery(txt) }
+                />
+                <Button
+                    onPress={ async () => {
+                        setIsLoading(true);
+                        let xx = await searchApi(searchQuery);
+                        console.debug("xx:",xx);
+                        setWordDefinition(xx.definitions)
+                        setIsLoading(false);
+                    }}
+                    title={"Search"}
+                />
 
-            {
-                (isLoading)
-                ? <Text>Loading...</Text>
-                : (wordDefinition && wordDefinition.length>0) ? 
-                    wordDefinition.map( (def, i) => (
-                        <React.Fragment key={i}>
-                            {/* <View> */}
-                                <CollapsedDefinition props={def} />
-                            {/* </View> */}
-                        </React.Fragment>
-                    ) )
-                    // <SafeAreaView style={styles.container}>
-                    //     <FlatList
-                    //         data={wordDefinition}
-                    //         renderItem={({ item }) => <CollapsedDefinition data={item} />}
-                    //         keyExtractor={ (item, i) => i.toString() }
-                    //     />
-                    //     </SafeAreaView>
-                    : <Text>""</Text>
+                {
+                    (isLoading)
+                    ? <Text>Loading...</Text>
+                    : (wordDefinition && wordDefinition.length>0) ? 
+                        wordDefinition.map( (def, i) => (
+                            <React.Fragment key={i}>
+                                {/* <View> */}
+                                    <CollapsedDefinition props={def} />
+                                {/* </View> */}
+                            </React.Fragment>
+                        ) )
+                        // <SafeAreaView style={styles.container}>
+                        //     <FlatList
+                        //         data={wordDefinition}
+                        //         renderItem={({ item }) => <CollapsedDefinition data={item} />}
+                        //         keyExtractor={ (item, i) => i.toString() }
+                        //     />
+                        //     </SafeAreaView>
+                        : <Text>""</Text>
+                        
                     
-                
-            }
+                }
 
-            <Button
-                onPress={() => navigation.navigate('Notifications')}
-                title="Go to notifications"
-            />
-        </View>
+                <Button
+                    onPress={() => navigation.navigate('Notifications')}
+                    title="Go to notifications"
+                />
+            </View>
         </ScrollView>
     )
 }
@@ -96,8 +96,8 @@ const logoutUser = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        // justifyContent: 'center',
+        // alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
     welcome: {
