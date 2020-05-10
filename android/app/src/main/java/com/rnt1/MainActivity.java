eb1d1306c623d2ac3854d43ad633f2a51ea208a4
kavindasilva <kss.kavinda@gmail.com>
2020-05-10 23:02:07 +0530

    WIP init2

--- android/app/src/main/java/com/rnt1/MainActivity.java
+++ android/app/src/main/java/com/rnt1/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rrr2;
+package com.rnt1;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "rrr2";
+    return "rnt1";
   }
 }
--- ios/rnt1Tests/rnt1Tests.m
+++ ios/rnt1Tests/rnt1Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rrr2Tests : XCTestCase
+@interface rnt1Tests : XCTestCase
 
 @end
 
-@implementation rrr2Tests
+@implementation rnt1Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
